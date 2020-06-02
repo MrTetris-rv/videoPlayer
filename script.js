8d5bfd09a930eb80578af6d48fd083759d04ff15
MrTetris-rv <sharailee@bk.ru>
2020-06-02 13:23:04 +0300

    up

---
+++ script.js
@@ -0,0 +1,28 @@
+vid = document.getElementById("media");
+player = document.getElementById("darkplayer");
+// vid.src = "v2.mp4";
+ok = document.getElementById("vi2");
+ok.addEventListener('click', () => {
+  vid.style.height = 403 + "px";
+  vid.src = "hi.mp4";
+});
+
+
+
+function vi1() {
+  vid.src = "v1.webm";
+
+}
+
+
+function vi3() {
+  vid.src = "video.mp4";
+}
+
+function vi4() {
+  vid.src = "v2.mp4";
+}
+
+function vi5() {
+  vid.src = "wt.mp4";
+}
